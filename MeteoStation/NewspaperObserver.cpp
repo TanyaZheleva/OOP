81c4fcfaa1f409c9e982837fb13943f92c68d19a
TanyaZheleva <44448449+TanyaZheleva@users.noreply.github.com>
2019-08-01 19:41:26 +0300

    Meteo Station

---
+++ MeteoStation/NewspaperObserver.cpp
@@ -0,0 +1,31 @@
+#include "NewspaperObserver.h"
+
+NewspaperObserver::NewspaperObserver(std::string _name, std::string _city, double _temperature)
+	:Observer(_name,_city,_temperature)
+	//:name(_name), city(_city), temperature(_temperature)
+{}
+
+NewspaperObserver::NewspaperObserver(const NewspaperObserver & old)
+	:Observer(old)
+{
+	/*name = old.name;
+	city = old.city;
+	temperature = old.temperature;*/
+}
+
+void NewspaperObserver::update(std::string _city, double _temperature)
+{
+	city = _city;
+	temperature = _temperature;
+	std::cout << "Update " << name << ": The temperature in " << city << " is " << temperature << "\n";
+}
+
+void NewspaperObserver::printMeteo() const
+{
+	std::cout << "Newspaper " << name << ": The temperature in " << city << " is " << temperature << '\n';
+}
+
+Observer * NewspaperObserver::clone()
+{
+	return new NewspaperObserver(*this);
+}
---
+++ MeteoStation/NewspaperObserver.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Observer.h"
+class NewspaperObserver :public Observer
+{
+public:
+	NewspaperObserver(std::string _name,std::string _city,double _temperature);
+	NewspaperObserver(const NewspaperObserver& old);
+	void update(std::string _city, double _temperature) override;
+	void printMeteo()const;
+	Observer* clone() override;
+//private:
+//	std::string name;
+//	std::string city;
+//	double temperature;
+};
+
